5d53c775b88d664c0e35ffbaa9cfe3db88e150cf
enemik <mikael.ene@gmail.com>
2019-07-02 08:45:44 +0200

    added get_columns_in_relation

--- dbt/include/sqlserver/macros/adapters.sql
+++ dbt/include/sqlserver/macros/adapters.sql
@@ -80,3 +80,23 @@
   getdate()
 {%- endmacro %}
 
+{% macro sqlserver__get_columns_in_relation(relation) -%}
+  {% call statement('get_columns_in_relation', fetch_result=True) %}
+      select
+          column_name,
+          data_type,
+          character_maximum_length,
+          numeric_precision,
+          numeric_scale
+
+      from INFORMATION_SCHEMA.COLUMNS
+      where table_name = '{{ relation.identifier }}'
+        {% if relation.schema %}
+        and table_schema = '{{ relation.schema }}'
+        {% endif %}
+      order by ordinal_position
+
+  {% endcall %}
+  {% set table = load_result('get_columns_in_relation').table %}
+  {{ return(sql_convert_columns_in_relation(table)) }}
+{% endmacro %}
