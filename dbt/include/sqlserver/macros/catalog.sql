1e744beb8afc39782fcd35729dc7c1ac8b60f9bb
mikaelene <mikael.ene@gmail.com>
2020-01-13 13:36:37 +0100

    Fix for issues #12. Docs doen't work on 0.15

--- dbt/include/sqlserver/macros/catalog.sql
+++ dbt/include/sqlserver/macros/catalog.sql
@@ -3,24 +3,15 @@
 
   {%- call statement('catalog', fetch_result=True) -%}
 
-    with table_owners as (
+    with tabs as (
 
 		select
 			TABLE_CATALOG as table_database,
 			TABLE_SCHEMA as table_schema,
 			TABLE_NAME as table_name,
-			TABLE_SCHEMA as table_owner
-		from INFORMATION_SCHEMA.TABLES
-
-    ),
-
-    tabs as (
-
-		select
-			TABLE_CATALOG as table_database,
-			TABLE_SCHEMA as table_schema,
-			TABLE_NAME as table_name,
-			TABLE_SCHEMA as table_owner
+			TABLE_TYPE as table_type,
+			TABLE_SCHEMA as table_owner,
+			null as table_comment
 		from INFORMATION_SCHEMA.TABLES
 
     ),
@@ -31,19 +22,27 @@
             table_catalog as table_database,
             table_schema,
             table_name,
-            null as table_comment,
             column_name,
             ordinal_position as column_index,
             data_type as column_type,
-            null as column_comment
+			null as column_comment
         from information_schema.columns
 
     )
 
-    select *
+    select
+		tabs.table_database,
+		tabs.table_schema,
+		tabs.table_name,
+		tabs.table_type,
+		tabs.table_comment,
+		tabs.table_owner,
+		cols.column_name,
+		cols.column_index,
+		cols.column_type,
+		cols.column_comment
     from tabs
     join cols on tabs.table_database = cols.table_database and tabs.table_schema = cols.table_schema and tabs.table_name = cols.table_name
-    join table_owners on tabs.table_database = table_owners.table_database and tabs.table_schema = table_owners.table_schema and tabs.table_name = table_owners.table_name
     order by column_index
 
   {%- endcall -%}
