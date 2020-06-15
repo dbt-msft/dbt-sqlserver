15df5265b30f09368f463ad4e9b1169b3cecee7e
mikael.ene <mikael.ene@gmail.com>
2020-06-15 08:33:07 +0200

    Added [] for USE database sql

--- dbt/include/sqlserver/macros/adapters.sql
+++ dbt/include/sqlserver/macros/adapters.sql
@@ -29,7 +29,7 @@
 
 {% macro sqlserver__create_schema(relation) -%}
   {% call statement('create_schema') -%}
-    USE {{ relation.database }}
+    USE [{{ relation.database }}]
     IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = '{{ relation.without_identifier().schema }}')
     BEGIN
     EXEC('CREATE SCHEMA {{ relation.without_identifier().schema }}')
