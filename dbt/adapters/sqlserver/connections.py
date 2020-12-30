0fc3afec39bf333fb64919a63e62f481635e4add
Cor Zuurmond <jczuurmond@protonmail.com>
2020-12-30 12:00:43 +0100

    Change line length

--- dbt/adapters/sqlserver/connections.py
+++ dbt/adapters/sqlserver/connections.py
@@ -320,24 +320,17 @@ class SQLServerConnectionManager(SQLConnectionManager):
         # return self.add_query('COMMIT TRANSACTION', auto_begin=False)
         pass
 
-    def add_query(
-        self, sql, auto_begin=True, bindings=None, abridge_sql_log=False
-    ):
+    def add_query(self, sql, auto_begin=True, bindings=None, abridge_sql_log=False):
 
         connection = self.get_thread_connection()
 
         if auto_begin and connection.transaction_open is False:
             self.begin()
 
-        logger.debug(
-            'Using {} connection "{}".'.format(self.TYPE, connection.name)
-        )
-
+        logger.debug('Using {} connection "{}".'.format(self.TYPE, connection.name))
         with self.exception_handler(sql):
             if abridge_sql_log:
-                logger.debug(
-                    "On {}: {}....".format(connection.name, sql[0:512])
-                )
+                logger.debug("On {}: {}....".format(connection.name, sql[0:512]))
             else:
                 logger.debug("On {}: {}".format(connection.name, sql))
             pre = time.time()
