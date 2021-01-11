a7d7bfbe6cd858b95dcbd081ffbe115025039404
mikaelene <mikael.ene@eneanalytics.com>
2021-01-11 14:23:34 +0100

    Bumbed dbt version

--- setup.py
+++ setup.py
@@ -56,5 +56,5 @@ setup(
             "include/sqlserver/macros/**/**/*.sql",
         ]
     },
-    install_requires=["dbt-core~=0.18.0", "pyodbc>=4.0.27", "azure-identity>=1.4.0"],
+    install_requires=["dbt-core~=0.19.0", "pyodbc>=4.0.27", "azure-identity>=1.4.0"],
 )
