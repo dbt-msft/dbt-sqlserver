1d5ca8b31b1efba31ca775dade82d20f66371f98
Anders Swanson <swanson.anders@gmail.com>
2021-02-15 22:35:43 -0800

    leave quoting as is

--- dbt/adapters/sqlserver/relation.py
+++ dbt/adapters/sqlserver/relation.py
@@ -6,11 +6,6 @@ from dbt.adapters.base.relation import BaseRelation, Policy
 from dbt.exceptions import RuntimeException
 
 
-@dataclass
-class SQLServerQuotePolicy(Policy):
-    database: bool = False
-    schema: bool = False
-    identifier: bool = False
 
 
 @dataclass
@@ -22,6 +17,5 @@ class SQLServerIncludePolicy(Policy):
 
 @dataclass(frozen=True, eq=False, repr=False)
 class SQLServerRelation(BaseRelation):
-    quote_policy: SQLServerQuotePolicy = SQLServerQuotePolicy()
     include_policy: SQLServerIncludePolicy = SQLServerIncludePolicy()
     quote_character: str = '"'
