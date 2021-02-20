e728ab06f5e831f87043eb317d9351b433e343cf
Cor Zuurmond <jczuurmond@protonmail.com>
2021-02-20 09:50:30 +0100

    Mock cli authentication

--- test/unit/adapters/sqlserver/test_connections.py
+++ test/unit/adapters/sqlserver/test_connections.py
@@ -1,6 +1,15 @@
+import datetime as dt
+import json
+from unittest import mock
+
 import pytest
+from azure.identity import AzureCliCredential
 
 from dbt.adapters.sqlserver import SQLServerCredentials, connections
+
+
+# See https://github.com/Azure/azure-sdk-for-python/blob/azure-identity_1.5.0/sdk/identity/azure-identity/tests/test_cli_credential.py
+CHECK_OUTPUT = AzureCliCredential.__module__ + ".subprocess.check_output"
 
 
 @pytest.fixture
@@ -12,6 +21,24 @@ def credentials() -> SQLServerCredentials:
         schema="sqlserver",
     )
     return credentials
+
+
+@pytest.fixture
+def mock_cli_access_token() -> str:
+    access_token = "access token"
+    expected_expires_on = 1602015811
+    successful_output = json.dumps(
+        {
+            "expiresOn": dt.datetime.fromtimestamp(
+                expected_expires_on
+            ).strftime("%Y-%m-%d %H:%M:%S.%f"),
+            "accessToken": access_token,
+            "subscription": "some-guid",
+            "tenant": "some-guid",
+            "tokenType": "Bearer",
+        }
+    )
+    return successful_output
 
 
 def test_get_pyodbc_attrs_before_empty_dict_when_service_principal(
@@ -27,12 +54,16 @@ def test_get_pyodbc_attrs_before_empty_dict_when_service_principal(
 @pytest.mark.parametrize("authentication", ["CLI", "cli", "cLi"])
 def test_get_pyodbc_attrs_before_contains_access_token_key_for_cli_authentication(
     credentials: SQLServerCredentials,
-    authentication: str
+    authentication: str,
+    mock_cli_access_token: str,
 ) -> None:
     """
     When the cli authentication is used, the attrs before should contain an
     access token key.
     """
     credentials.authentication = authentication
-    attrs_before = connections.get_pyodbc_attrs_before(credentials)
+    with mock.patch(
+        CHECK_OUTPUT, mock.Mock(return_value=mock_cli_access_token)
+    ):
+        attrs_before = connections.get_pyodbc_attrs_before(credentials)
     assert 1256 in attrs_before.keys()
