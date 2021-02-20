02ca2a83b8e10e87d877e1fac33b52f393fe6453
Cor Zuurmond <jczuurmond@protonmail.com>
2021-02-20 09:24:51 +0100

    Add test for when authentication is CLI

--- test/unit/adapters/sqlserver/test_connections.py
+++ test/unit/adapters/sqlserver/test_connections.py
@@ -22,3 +22,15 @@ def test_get_pyodbc_attrs_before_empty_dict_when_service_principal(
     """
     attrs_before = connections.get_pyodbc_attrs_before(credentials)
     assert attrs_before == dict()
+
+
+def test_get_pyodbc_attrs_before_contains_access_token_key(
+    credentials: SQLServerCredentials,
+) -> None:
+    """
+    When the Azure authentication is used, the attrs before should contain an
+    access token key.
+    """
+    credentials.authentication = "CLI"
+    attrs_before = connections.get_pyodbc_attrs_before(credentials)
+    assert 1256 in attrs_before.keys()
