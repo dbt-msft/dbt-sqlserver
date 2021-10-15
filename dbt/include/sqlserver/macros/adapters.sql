515b60f9733fd7f7ecc498f475dd8a186e2a9351
David Larsen <david.larsen@emboldhealth.com>
2021-10-15 15:25:23 -0400

    add sqlserver__alter_column_type macro

--- dbt/include/sqlserver/macros/adapters.sql
+++ dbt/include/sqlserver/macros/adapters.sql
@@ -225,3 +225,18 @@
     {%- set result = "CONVERT(DATETIME2, '" ~ timestamp ~ "')" -%}
     {{ return(result) }}
 {%- endmacro %}
+
+{% macro sqlserver__alter_column_type(relation, column_name, new_column_type) %}
+
+  {%- set tmp_column = column_name + "__dbt_alter" -%}
+
+  {% call statement('alter_column_type') -%}
+
+    alter {{ relation.type }} {{ relation }} add {{ tmp_column }} {{ new_column_type }};
+    update {{ relation }} set {{ tmp_column }} = {{ column_name }};
+    alter {{ relation.type }} {{ relation }} drop column {{ column_name }};
+    exec sp_rename '{{ relation | replace('"', '') }}.{{ tmp_column }}', '{{ column_name }}', 'column'
+
+  {%- endcall -%}
+
+{% endmacro %}
