7e92daa05209a98f87c6d54ff14acf31c8f2e585
swanderz <swanson.anders@gmail.com>
2021-11-05 17:33:29 -0700

    always store results; but drop them unless specified to store

--- dbt/include/sqlserver/macros/materializations/test.sql
+++ dbt/include/sqlserver/macros/materializations/test.sql
@@ -15,35 +15,26 @@
 
   {% set relations = [] %}
 
-  {% if should_store_failures() %}
+  {% set identifier = model['alias'] %}
+  {% set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) %}
+  {% set target_relation = api.Relation.create(
+      identifier=identifier, schema=schema, database=database, type='table') -%} %}
 
-    {% set identifier = model['alias'] %}
-    {% set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) %}
-    {% set target_relation = api.Relation.create(
-        identifier=identifier, schema=schema, database=database, type='table') -%} %}
-    
-    {% if old_relation %}
-        {% do adapter.drop_relation(old_relation) %}
-    {% endif %}
-    
-    {% call statement(auto_begin=True) %}
-        {{ create_table_as(False, target_relation, sql) }}
-    {% endcall %}
-    
-    {% do relations.append(target_relation) %}
-  
-    {% set main_sql %}
-        select *
-        from {{ target_relation }}
-    {% endset %}
-    
-    {{ adapter.commit() }}
-  
-  {% else %}
-
-      {% set main_sql = sql %}
-  
+  {% if old_relation %}
+      {% do adapter.drop_relation(old_relation) %}
   {% endif %}
+  
+  {% call statement(auto_begin=True) %}
+      {{ create_table_as(False, target_relation, sql) }}
+  {% endcall %}
+
+  {% set main_sql %}
+      select *
+      from {{ target_relation }}
+  {% endset %}
+  
+  {{ adapter.commit() }}
+  
 
   {% set limit = config.get('limit') %}
   {% set fail_calc = config.get('fail_calc') %}
@@ -55,6 +46,12 @@
     {{ get_test_sql(main_sql, fail_calc, warn_if, error_if, limit)}}
 
   {%- endcall %}
+
+  {% if should_store_failures() %}
+    {% do relations.append(target_relation) %}
+  {% elif not should_store_failures() %}
+    {% do adapter.drop_relation(target_relation) %}
+  {% endif %}
   
   {{ return({'relations': relations}) }}
 
