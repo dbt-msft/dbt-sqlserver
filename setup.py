dde71162d20f141fde5f47ecf42eae8eabfb1677
swanderz <swanson.anders@gmail.com>
2021-12-15 15:11:48 -0800

    bump version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     packages=find_namespace_packages(include=['dbt', 'dbt.*']),
     include_package_data=True,
     install_requires=[
-        "dbt-core~=1.0.0rc1",
+        "dbt-core~=1.0.0",
         "pyodbc~=4.0.32",
         "azure-identity>=1.7.0",
     ]
