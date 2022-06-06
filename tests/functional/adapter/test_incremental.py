2f050f65c4a697b4bc58df636dab6c06344a98fd
Sam Debruyn <sam@debruyn.dev>
2022-06-06 23:45:46 +0200

    use tests in dbt-core

--- tests/functional/adapter/test_incremental.py
+++ tests/functional/adapter/test_incremental.py
@@ -1,30 +1,5 @@
-import pytest
-from dbt.tests.util import run_dbt
+from dbt.tests.adapter.incremental.test_incremental_unique_id import BaseIncrementalUniqueKey
 
 
-class TestAdditionalIncrementalSQLServer:
-    #  https://github.com/dbt-msft/dbt-sqlserver/issues/241
-
-    @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {"name": "issue_241", "models": {"+materialized": "incremental"}}
-
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {
-            "incremental_unique_array.sql": """
-    {{
-      config ( unique_key=['A', 'B'] )
-    }}
-    WITH CTE AS
-    (
-      SELECT 1 AS A, 2 AS B
-      UNION
-      SELECT 3 AS A, 4 AS B
-    )
-    SELECT A, B FROM CTE""",
-        }
-
-    def test_run_incremental(self, project):
-        run_dbt(["run"])
-        run_dbt(["run"])
+class TestBaseIncrementalUniqueKeySQLServer(BaseIncrementalUniqueKey):
+    pass
