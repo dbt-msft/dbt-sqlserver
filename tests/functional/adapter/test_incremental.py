153d80adc5d67f95ddd5ccf18d3af4a956743eeb
Sam Debruyn <sam@debruyn.dev>
2022-06-06 15:56:07 +0200

    add test for incremental model with unique array

---
+++ tests/functional/adapter/test_incremental.py
@@ -0,0 +1,33 @@
+import pytest
+from dbt.tests.util import run_dbt
+
+
+class TestAddtionalIncrementalSQLServer:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            # https://github.com/dbt-msft/dbt-sqlserver/issues/241
+            "incremental_unique_array.sql": """
+    {{
+      config
+      (
+        materialized='incremental'
+        , unique_key=['A', 'B']
+      )
+    }}
+    WITH CTE AS
+    (
+      SELECT 1 AS A, 2 AS B
+      UNION
+      SELECT 3 AS A, 4 AS B
+    )
+    SELECT
+      A
+      , B
+    FROM
+      CTE
+            """,
+        }
+
+    def test_build(self, project):
+        run_dbt(["build"])
