ec6a8629a0ee80136585f11c8b09cd52173c7b60
Sam Debruyn <sam@debruyn.dev>
2022-06-06 22:49:22 +0200

    repro failure and add dbt core macro

--- tests/functional/adapter/test_incremental.py
+++ tests/functional/adapter/test_incremental.py
@@ -3,17 +3,18 @@ from dbt.tests.util import run_dbt
 
 
 class TestAdditionalIncrementalSQLServer:
+    #  https://github.com/dbt-msft/dbt-sqlserver/issues/241
+
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {"name": "issue_241", "models": {"+materialized": "incremental"}}
+
     @pytest.fixture(scope="class")
     def models(self):
         return {
-            # https://github.com/dbt-msft/dbt-sqlserver/issues/241
             "incremental_unique_array.sql": """
     {{
-      config
-      (
-        materialized='incremental'
-        , unique_key=['A', 'B']
-      )
+      config ( unique_key=['A', 'B'] )
     }}
     WITH CTE AS
     (
@@ -21,13 +22,9 @@ class TestAdditionalIncrementalSQLServer:
       UNION
       SELECT 3 AS A, 4 AS B
     )
-    SELECT
-      A
-      , B
-    FROM
-      CTE
-            """,
+    SELECT A, B FROM CTE""",
         }
 
     def test_build(self, project):
-        run_dbt(["build"])
+        run_dbt(["run"])
+        run_dbt(["run"])
