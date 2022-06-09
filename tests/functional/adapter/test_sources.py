607cc19146dd1d9664cf203cffd2168dd36516ac
Chris Baldwin <c1.baldwin@qut.edu.au>
2022-06-09 10:25:17 +0000

    simplest possible test for freshness and timestamp vs datetimeoffset

--- tests/functional/adapter/test_sources.py
+++ tests/functional/adapter/test_sources.py
@@ -28,6 +28,23 @@ sources:
       - not_null
 """
 
+source_freshness = """
+version: 2
+sources:
+- name: freshness
+  error_after:
+    count: 1
+    period: minute
+  schema: INFORMATION_SCHEMA
+  tables:
+  - name: freshness_timestamp
+    loaded_at_field: CAST(current_timestamp AS timestamp)
+    identifier: VIEWS
+  - name: freshness_datetimeoffset
+    identifier: VIEWS
+    loaded_at_field: CAST(current_timestamp AS datetimeoffset)
+"""
+
 select_from_source_regular = """
 select * from {{ source("regular", "VIEWS") }}
 """
@@ -36,13 +53,13 @@ select_from_source_space_in_name = """
 select * from {{ source("space in name", "VIEWS") }}
 """
 
-
 class TestSourcesSQLServer:
     @pytest.fixture(scope="class")
     def models(self):
         return {
             "source_regular.yml": source_regular,
             "source_space_in_name.yml": source_space_in_name,
+            "source_freshness.yml": source_freshness,
             "v_select_from_source_regular.sql": config_materialized_view
             + select_from_source_regular,
             "v_select_from_source_space_in_name.sql": config_materialized_view
@@ -63,9 +80,10 @@ class TestSourcesSQLServer:
         run_dbt(["compile"])
 
         ls = run_dbt(["list"])
-        assert len(ls) == 8
+        assert len(ls) == 10
         ls_sources = [src for src in ls if src.startswith("source:")]
-        assert len(ls_sources) == 2
+        assert len(ls_sources) == 3
 
         run_dbt(["run"])
         run_dbt(["test"])
+        run_dbt(["source", "freshness"])
