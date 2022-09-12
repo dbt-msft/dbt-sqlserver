e6cfa3abca68d762595ce01fcb12f269973edffe
Sam Debruyn <sam@debruyn.dev>
2022-09-12 12:02:07 +0200

    add new basic tests

--- tests/functional/adapter/test_basic.py
+++ tests/functional/adapter/test_basic.py
@@ -1,6 +1,7 @@
 import pytest
 from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
 from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
+from dbt.tests.adapter.basic.test_docs_generate import BaseDocsGenerate, BaseDocsGenReferences
 from dbt.tests.adapter.basic.test_empty import BaseEmpty
 from dbt.tests.adapter.basic.test_ephemeral import BaseEphemeral
 from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
@@ -9,6 +10,7 @@ from dbt.tests.adapter.basic.test_singular_tests import BaseSingularTests
 from dbt.tests.adapter.basic.test_singular_tests_ephemeral import BaseSingularTestsEphemeral
 from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
 from dbt.tests.adapter.basic.test_snapshot_timestamp import BaseSnapshotTimestamp
+from dbt.tests.adapter.basic.test_validate_connection import BaseValidateConnection
 
 
 class TestSimpleMaterializationsSQLServer(BaseSimpleMaterializations):
@@ -48,5 +50,17 @@ class TestSnapshotTimestampSQLServer(BaseSnapshotTimestamp):
     pass
 
 
-class TestBaseAdapterMethodSQLServer(BaseAdapterMethod):
+class TestBaseCachingSQLServer(BaseAdapterMethod):
+    pass
+
+
+class TestDocsGenerateSQLServer(BaseDocsGenerate):
+    pass
+
+
+class TestDocsGenReferencesSQLServer(BaseDocsGenReferences):
+    pass
+
+
+class TestValidateConnectionSQLServer(BaseValidateConnection):
     pass
