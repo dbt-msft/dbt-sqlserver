e6716c1b2ac1407c2679048dd07fc98683264847
Sam Debruyn <sam@debruyn.dev>
2022-09-12 16:50:55 +0200

    move docs tests to sep. file and fix
    remove order-by from cte and view and fix datatypes

--- tests/functional/adapter/test_basic.py
+++ tests/functional/adapter/test_basic.py
@@ -1,7 +1,6 @@
 import pytest
 from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
 from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
-from dbt.tests.adapter.basic.test_docs_generate import BaseDocsGenerate, BaseDocsGenReferences
 from dbt.tests.adapter.basic.test_empty import BaseEmpty
 from dbt.tests.adapter.basic.test_ephemeral import BaseEphemeral
 from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
@@ -51,14 +50,6 @@ class TestSnapshotTimestampSQLServer(BaseSnapshotTimestamp):
 
 
 class TestBaseCachingSQLServer(BaseAdapterMethod):
-    pass
-
-
-class TestDocsGenerateSQLServer(BaseDocsGenerate):
-    pass
-
-
-class TestDocsGenReferencesSQLServer(BaseDocsGenReferences):
     pass
 
 
