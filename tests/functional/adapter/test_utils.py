6d90239e9f8f22d55823013741a26b33640fc91b
Sam Debruyn <sam@debruyn.dev>
2022-09-12 21:31:23 +0200

    implement listagg macro

--- tests/functional/adapter/test_utils.py
+++ tests/functional/adapter/test_utils.py
@@ -1,5 +1,9 @@
 import pytest
 from dbt.tests.adapter.utils.fixture_cast_bool_to_text import models__test_cast_bool_to_text_yml
+from dbt.tests.adapter.utils.fixture_listagg import (
+    models__test_listagg_yml,
+    seeds__data_listagg_csv,
+)
 from dbt.tests.adapter.utils.test_any_value import BaseAnyValue
 from dbt.tests.adapter.utils.test_bool_or import BaseBoolOr
 from dbt.tests.adapter.utils.test_cast_bool_to_text import BaseCastBoolToText
@@ -100,7 +104,86 @@ class TestLengthSQLServer(BaseLength):
 
 
 class TestListaggSQLServer(BaseListagg):
-    pass
+    #  Only supported in SQL Server 2017 and later or cloud versions
+    #  DISTINCT not supported
+    #  limit not supported
+
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        seeds__data_listagg_output_csv = """group_col,expected,version
+1,"a_|_b_|_c",bottom_ordered
+2,"1_|_a_|_p",bottom_ordered
+3,"g_|_g_|_g",bottom_ordered
+3,"g, g, g",comma_whitespace_unordered
+3,"g,g,g",no_params
+        """
+
+        return {
+            "data_listagg.csv": seeds__data_listagg_csv,
+            "data_listagg_output.csv": seeds__data_listagg_output_csv,
+        }
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        models__test_listagg_sql = """
+with data as (
+
+    select * from {{ ref('data_listagg') }}
+
+),
+
+data_output as (
+
+    select * from {{ ref('data_listagg_output') }}
+
+),
+
+calculate as (
+
+    select
+        group_col,
+        {{ listagg('string_text', "'_|_'", "order by order_col") }} as actual,
+        'bottom_ordered' as version
+    from data
+    group by group_col
+
+    union all
+
+    select
+        group_col,
+        {{ listagg('string_text', "', '") }} as actual,
+        'comma_whitespace_unordered' as version
+    from data
+    where group_col = 3
+    group by group_col
+
+    union all
+
+    select
+        group_col,
+        {{ listagg('string_text') }} as actual,
+        'no_params' as version
+    from data
+    where group_col = 3
+    group by group_col
+
+)
+
+select
+    calculate.actual,
+    data_output.expected
+from calculate
+left join data_output
+on calculate.group_col = data_output.group_col
+and calculate.version = data_output.version
+"""
+
+        return {
+            "test_listagg.yml": models__test_listagg_yml,
+            "test_listagg.sql": self.interpolate_macro_namespace(
+                models__test_listagg_sql, "listagg"
+            ),
+        }
 
 
 class TestRightSQLServer(BaseRight):
