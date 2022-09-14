73b16e1d29303662c67aad3ec1b98373626d50e6
Sam Debruyn <sam@debruyn.dev>
2022-09-14 22:43:48 +0200

    add new test from core

--- tests/functional/adapter/test_basic.py
+++ tests/functional/adapter/test_basic.py
@@ -4,7 +4,10 @@ from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
 from dbt.tests.adapter.basic.test_empty import BaseEmpty
 from dbt.tests.adapter.basic.test_ephemeral import BaseEphemeral
 from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
-from dbt.tests.adapter.basic.test_incremental import BaseIncremental
+from dbt.tests.adapter.basic.test_incremental import (
+    BaseIncremental,
+    BaseIncrementalNotSchemaChange,
+)
 from dbt.tests.adapter.basic.test_singular_tests import BaseSingularTests
 from dbt.tests.adapter.basic.test_singular_tests_ephemeral import BaseSingularTestsEphemeral
 from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
@@ -34,6 +37,10 @@ class TestEphemeralSQLServer(BaseEphemeral):
 
 
 class TestIncrementalSQLServer(BaseIncremental):
+    pass
+
+
+class TestIncrementalNotSchemaChangeSQLServer(BaseIncrementalNotSchemaChange):
     pass
 
 
