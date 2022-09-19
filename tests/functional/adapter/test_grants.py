f873cf970ad1ee1224dd4b55b7f023e6027dbf06
Sam Debruyn <sam@debruyn.dev>
2022-09-19 11:02:41 +0200

    errors on azure are different

--- tests/functional/adapter/test_grants.py
+++ tests/functional/adapter/test_grants.py
@@ -1,3 +1,4 @@
+import pytest
 from dbt.tests.adapter.grants.test_incremental_grants import BaseIncrementalGrants
 from dbt.tests.adapter.grants.test_invalid_grants import BaseInvalidGrants
 from dbt.tests.adapter.grants.test_model_grants import BaseModelGrants
@@ -9,9 +10,19 @@ class TestIncrementalGrantsSQLServer(BaseIncrementalGrants):
     pass
 
 
+@pytest.mark.only_with_profile("user", "ci_sql_server")
 class TestInvalidGrantsSQLServer(BaseInvalidGrants):
     def grantee_does_not_exist_error(self):
         return "Cannot find the user"
+
+    def privilege_does_not_exist_error(self):
+        return "Incorrect syntax near"
+
+
+@pytest.mark.skip_profile("user", "ci_sql_server")
+class TestInvalidGrantsSQLServerAzure(BaseInvalidGrants):
+    def grantee_does_not_exist_error(self):
+        return "Principal 'invalid_user' could not be found"
 
     def privilege_does_not_exist_error(self):
         return "Incorrect syntax near"
