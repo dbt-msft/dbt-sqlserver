d3e8f128fc6949be2ae9f5fa31932aa4922fc5b5
Sam Debruyn <sam@debruyn.dev>
2022-10-05 21:54:22 +0200

    remove datediff macro

--- dbt/include/sqlserver/macros/utils/datediff.sql
+++
@@ -1,7 +0,0 @@
-{% macro synapse__datediff(first_date, second_date, datepart) %}
-    datediff(
-        {{ datepart }},
-        cast({{first_date}} as datetime),
-        cast({{second_date}} as datetime)
-        )
-{% endmacro %}
