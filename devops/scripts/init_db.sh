fe5e508daf676e841b19502d87903520c6b84454
Sam Debruyn <sam@debruyn.dev>
2022-10-06 16:11:48 +0200

    change compatibility level in Docker image

--- devops/scripts/init_db.sh
+++ devops/scripts/init_db.sh
@@ -2,10 +2,10 @@
 
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SA_PASSWORD}" -d msdb -I -i create_sql_users.sql
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SA_PASSWORD}" -d msdb -I -i init.sql
     if [ $? -eq 0 ]
     then
-        echo "create_sql_users.sql completed"
+        echo "init.sql completed"
         break
     else
         echo "not ready yet..."
