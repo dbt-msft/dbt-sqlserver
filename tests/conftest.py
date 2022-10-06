674c9b1b103fcc46974e177a017d90fa5ee9a41d
Sam Debruyn <sam@debruyn.dev>
2022-10-06 16:38:11 +0200

    fix path to SQL init script

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ pytest_plugins = ["dbt.tests.fixtures.project"]
 
 
 def pytest_addoption(parser):
-    parser.addoption("--profile", action="store", default="user", type=str)
+    parser.addoption("--profile", action="store", default="user_azure", type=str)
 
 
 @pytest.fixture(scope="class")
