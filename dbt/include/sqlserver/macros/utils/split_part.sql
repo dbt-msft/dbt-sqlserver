0dd750eacf57cbd13c021e6c771d24106448ad32
Sam Debruyn <sam@debruyn.dev>
2022-10-06 15:46:21 +0200

    change split_part so that it works in Synapse as well

--- dbt/include/sqlserver/macros/utils/split_part.sql
+++ dbt/include/sqlserver/macros/utils/split_part.sql
@@ -1,9 +1,5 @@
-{#
-	For more information on how this XML trick works with splitting strings, see https://www.mssqltips.com/sqlservertip/1771/splitting-delimited-strings-using-xml-in-sql-server/
-#}
-
 {% macro sqlserver__split_part(string_text, delimiter_text, part_number) %}
 
-    LTRIM(CAST(('<X>'+REPLACE({{ string_text }},{{ delimiter_text }} ,'</X><X>')+'</X>') AS XML).value('(/X)[{{ part_number }}]', 'VARCHAR(128)'))
+    (select value from string_split({{ string_text }}, {{ delimiter_text }}, 1) where ordinal = {{ part_number }})
 
 {% endmacro %}
