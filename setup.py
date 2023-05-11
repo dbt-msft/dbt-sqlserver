6869c21818ebc90532d9d25e56f0122ff03d15b9
Anders <anders.swanson@dbtlabs.com>
2023-05-11 11:49:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
-        "dbt-core~=1.4.0",
+        "dbt-core~=1.4.5",
         "pyodbc~=4.0.35,!=4.0.36,!=4.0.37",
         "azure-identity>=1.12.0",
     ],
