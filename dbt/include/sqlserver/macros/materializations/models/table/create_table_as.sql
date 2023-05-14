305f6683f1eb81d8e26241afea190fb8f979c5e7
Sam Debruyn <sam@debruyn.dev>
2023-05-14 19:43:22 +0200

    stick closer to default impl for create_x_as

--- dbt/include/sqlserver/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/sqlserver/macros/materializations/models/table/create_table_as.sql
@@ -1,26 +1,43 @@
 {% macro sqlserver__create_table_as(temporary, relation, sql) -%}
+   {#- TODO: add contracts here when in dbt 1.5 -#}
+   {%- set sql_header = config.get('sql_header', none) -%}
    {%- set as_columnstore = config.get('as_columnstore', default=true) -%}
-   {% set tmp_relation = relation.incorporate(
-   path={"identifier": relation.identifier.replace("#", "") ~ '_temp_view'},
-   type='view')-%}
    {%- set temp_view_sql = sql.replace("'", "''") -%}
+   {%- set tmp_relation = relation.incorporate(
+        path={"identifier": relation.identifier.replace("#", "") ~ '_temp_view'},
+        type='view') -%}
 
-   {{ sqlserver__drop_relation_script(tmp_relation) }}
+   {{- sql_header if sql_header is not none -}}
 
-   {{ sqlserver__drop_relation_script(relation) }}
+    -- drop previous temp view
+   {{- sqlserver__drop_relation_script(tmp_relation) }}
 
+    -- create temp view
    USE [{{ relation.database }}];
    EXEC('create view {{ tmp_relation.include(database=False) }} as
-    {{ temp_view_sql }}
+    {{- temp_view_sql -}}
     ');
 
-   SELECT * INTO {{ relation }} FROM
-    {{ tmp_relation }}
+    -- drop current version of the table
+   {{- sqlserver__drop_relation_script(relation) -}}
 
+   -- select into the table and create it that way
+   {#- see https://learn.microsoft.com/en-us/sql/t-sql/queries/select-into-clause-transact-sql?view=sql-server-ver16#b-inserting-rows-using-minimal-logging #}
+   ALTER DATABASE [{{ relation.database }}] SET RECOVERY BULK_LOGGED
+
+   SELECT *
+   INTO {% if temporary %}#{% endif %}{{ relation.include(database=(not temporary), schema=(not temporary)) }}
+   FROM {{ tmp_relation }}
+
+   {#- see https://learn.microsoft.com/en-us/sql/t-sql/queries/select-into-clause-transact-sql?view=sql-server-ver16#b-inserting-rows-using-minimal-logging #}
+   ALTER DATABASE [{{ relation.database }}] SET RECOVERY FULL
+
+   -- drop temp view
    {{ sqlserver__drop_relation_script(tmp_relation) }}
 
-   {% if not temporary and as_columnstore -%}
-   {{ sqlserver__create_clustered_columnstore_index(relation) }}
-   {% endif %}
+   {%- if not temporary and as_columnstore -%}
+        -- add columnstore index
+        {{ sqlserver__create_clustered_columnstore_index(relation) }}
+   {%- endif -%}
 
 {% endmacro %}
