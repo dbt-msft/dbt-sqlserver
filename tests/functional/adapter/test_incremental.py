7502c8ba781e857633e6d57e582274cc17f13524
Sam Debruyn <sam@debruyn.dev>
2023-05-15 13:51:49 +0200

    add tests for new incremental features

--- tests/functional/adapter/test_incremental.py
+++ tests/functional/adapter/test_incremental.py
@@ -1,5 +1,24 @@
+import pytest
+from dbt.tests.adapter.incremental.test_incremental_on_schema_change import (
+    BaseIncrementalOnSchemaChange,
+)
+from dbt.tests.adapter.incremental.test_incremental_predicates import BaseIncrementalPredicates
 from dbt.tests.adapter.incremental.test_incremental_unique_id import BaseIncrementalUniqueKey
 
 
 class TestBaseIncrementalUniqueKeySQLServer(BaseIncrementalUniqueKey):
     pass
+
+
+class TestIncrementalOnSchemaChangeSQLServer(BaseIncrementalOnSchemaChange):
+    pass
+
+
+class TestIncrementalPredicatesDeleteInsertSQLServer(BaseIncrementalPredicates):
+    pass
+
+
+class TestPredicatesDeleteInsertSQLServer(BaseIncrementalPredicates):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {"models": {"+predicates": ["id != 2"], "+incremental_strategy": "delete+insert"}}
