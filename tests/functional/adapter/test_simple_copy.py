d5f109cfbd03a24d3a2de88ca45d1106b76f78cd
Sam Debruyn <sam@debruyn.dev>
2023-05-18 15:49:23 +0200

    wip

---
+++ tests/functional/adapter/test_simple_copy.py
@@ -0,0 +1,9 @@
+from dbt.tests.adapter.simple_copy.test_simple_copy import EmptyModelsArentRunBase, SimpleCopyBase
+
+
+class TestSimpleCopyBaseSQLServer(SimpleCopyBase):
+    pass
+
+
+class TestEmptyModelsArentRunSQLServer(EmptyModelsArentRunBase):
+    pass
