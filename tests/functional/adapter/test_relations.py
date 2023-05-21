53071bc44e432a2134542c21b91449d7da9aa20e
Sam Debruyn <sam@debruyn.dev>
2023-05-21 20:48:39 +0200

    add new relations tests

---
+++ tests/functional/adapter/test_relations.py
@@ -0,0 +1,7 @@
+from tests.adapter.dbt.tests.adapter.relations.test_changing_relation_type import (
+    BaseChangeRelationTypeValidator,
+)
+
+
+class TestChangeRelationTypesSQLServer(BaseChangeRelationTypeValidator):
+    pass
