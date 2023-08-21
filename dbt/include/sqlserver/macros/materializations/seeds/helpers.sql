0b828b101817d385bd3546ef7ab93d7dba50a93e
Lucas Ortiz <lucas.l.ortiz@gmail.com>
2023-08-21 19:33:12 +0200

    fix calc_batch_size #407

--- dbt/include/sqlserver/macros/materializations/seeds/helpers.sql
+++ dbt/include/sqlserver/macros/materializations/seeds/helpers.sql
@@ -8,12 +8,12 @@
 
 {% macro calc_batch_size(num_columns) %}
     {#
-        SQL Server allows for a max of 2100 parameters in a single statement.
+        SQL Server allows for a max of 2098 parameters in a single statement.
         Check if the max_batch_size fits with the number of columns, otherwise
         reduce the batch size so it fits.
     #}
     {% set max_batch_size = get_batch_size() %}
-    {% set calculated_batch = (2100 / num_columns)|int %}
+    {% set calculated_batch = (2098 / num_columns)|int %}
     {% set batch_size = [max_batch_size, calculated_batch] | min %}
 
     {{ return(batch_size) }}
