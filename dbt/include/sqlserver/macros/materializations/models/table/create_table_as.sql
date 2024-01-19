6951140de21b1e927c0aec8ac067afb29bff7e3e
Dominik Jagoda <djagoda@dyvenia.com>
2024-01-19 17:50:39 +0100

    [feature] Added `sql header` to `config`

--- dbt/include/sqlserver/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/sqlserver/macros/materializations/models/table/create_table_as.sql
@@ -1,6 +1,7 @@
 {% macro sqlserver__create_table_as(temporary, relation, sql) -%}
    {%- set as_columnstore = config.get('as_columnstore', default=true) -%}
    {%- set option_clause  = config.get('option_clause') -%}
+   {%- set sql_header = config.get('sql_header') -%}
    {% set tmp_relation = relation.incorporate(
    path={"identifier": relation.identifier.replace("#", "") ~ '_temp_view'},
    type='view')-%}
@@ -14,6 +15,10 @@
    EXEC('create view {{ tmp_relation.include(database=False) }} as
     {{ temp_view_sql }}
     ');
+    
+   {% if sql_header %}
+      {{ sql_header }} 
+   {% endif %}
 
    SELECT * INTO {{ relation }} FROM
     {{ tmp_relation }}
