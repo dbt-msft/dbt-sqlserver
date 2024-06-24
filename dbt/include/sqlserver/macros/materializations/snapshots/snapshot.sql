196647707202388f3590f7d8057f844970586e84
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-06-24 01:58:13 -0600

    Simplify for scalability
    add syntax works on sql server, but not on fabric, its expensive to do drops of big tables

--- dbt/include/sqlserver/macros/materializations/snapshots/snapshot.sql
+++ dbt/include/sqlserver/macros/materializations/snapshots/snapshot.sql
@@ -1,52 +1,10 @@
-{#
-Fabric uses the 'CREATE TABLE XYZ AS SELECT * FROM ABC' syntax to create tables.
-SQL Server doesnt support this, so we use the 'SELECT * INTO XYZ FROM ABC' syntax instead.
-#}
-
 {% macro sqlserver__create_columns(relation, columns) %}
   {# default__ macro uses "add column"
      TSQL preferes just "add"
   #}
-
-  {% set columns %}
-    {% for column in columns %}
-      , CAST(NULL AS {{column.data_type}}) AS {{ column.quoted }}
-    {% endfor %}
-  {% endset %}
-
-  {% set tempTableName %}
-    [{{relation.database}}].[{{ relation.schema }}].[{{ relation.identifier }}_{{ range(1300, 19000) | random }}]
-  {% endset %}
-
-  {% set tempTable %}
-        SELECT * {{columns}} INTO {{tempTableName}} FROM [{{relation.database}}].[{{ relation.schema }}].[{{ relation.identifier }}] {{ information_schema_hints() }}
-  {% endset %}
-
-  {% call statement('create_temp_table') -%}
-      {{ tempTable }}
-  {%- endcall %}
-
-  {% set dropTable %}
-      DROP TABLE [{{relation.database}}].[{{ relation.schema }}].[{{ relation.identifier }}]
-  {% endset %}
-
-  {% call statement('drop_table') -%}
-      {{ dropTable }}
-  {%- endcall %}
-
-  {% set createTable %}
-      SELECT * INTO {{ relation }} FROM {{tempTableName}} {{ information_schema_hints() }}
-  {% endset %}
-
-  {% call statement('create_Table') -%}
-      {{ createTable }}
-  {%- endcall %}
-
-  {% set dropTempTable %}
-      DROP TABLE {{tempTableName}}
-  {% endset %}
-
-  {% call statement('drop_temp_table') -%}
-      {{ dropTempTable }}
-  {%- endcall %}
+  {% for column in columns %}
+    {% call statement() %}
+      alter table {{ relation }} add "{{ column.name }}" {{ column.data_type }};
+    {% endcall %}
+  {% endfor %}
 {% endmacro %}
