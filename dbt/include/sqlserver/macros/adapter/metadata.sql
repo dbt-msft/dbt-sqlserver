9140793706f99aa8efb43154707f97a4b4ab9bec
Cody Scott <coscott@regionofwaterloo.ca>
2024-09-14 18:07:10 +0000

    restored nolock behaviour

--- dbt/include/sqlserver/macros/adapter/metadata.sql
+++ dbt/include/sqlserver/macros/adapter/metadata.sql
@@ -3,3 +3,6 @@
     {%- set query_label = config.get('query_tag','dbt-sqlserver') -%}
     OPTION (LABEL = '{{query_label}}');
 {% endmacro %}
+
+{% macro default__information_schema_hints() %}{% endmacro %}
+{% macro sqlserver__information_schema_hints() %}with (nolock){% endmacro %}
