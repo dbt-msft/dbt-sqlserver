5bf0bcad73b7482a379613fc104b662561787b76
Cody Scott <coscott@regionofwaterloo.ca>
2024-09-18 00:33:04 +0000

    updated test logic and tests

--- dbt/include/sqlserver/macros/materializations/tests.sql
+++ dbt/include/sqlserver/macros/materializations/tests.sql
@@ -7,40 +7,23 @@
     EXEC('CREATE SCHEMA [{{ target.schema }}]')
   END
 
-  {% set with_statement_pattern = 'with .+ as\s*\(' %}
-  {% set re = modules.re %}
-  {% set is_match = re.search(with_statement_pattern, main_sql, re.IGNORECASE) %}
+  {% set testview %}
+    [{{ target.schema }}.testview_{{ range(1300, 19000) | random }}]
+  {% endset %}
 
-  {% if is_match %}
-    {% set testview %}
-      [{{ target.schema }}.testview_{{ range(1300, 19000) | random }}]
-    {% endset %}
+  {% set sql = main_sql.replace("'", "''")%}
+  EXEC('create view {{testview}} as {{ sql }};')
+  select
+    {{ "top (" ~ limit ~ ')' if limit != none }}
+    {{ fail_calc }} as failures,
+    case when {{ fail_calc }} {{ warn_if }}
+      then 'true' else 'false' end as should_warn,
+    case when {{ fail_calc }} {{ error_if }}
+      then 'true' else 'false' end as should_error
+  from (
+    select * from {{testview}}
+  ) dbt_internal_test;
 
-    {% set sql = main_sql.replace("'", "''")%}
-    EXEC('create view {{testview}} as {{ sql }};')
-    select
-      {{ "top (" ~ limit ~ ')' if limit != none }}
-      {{ fail_calc }} as failures,
-      case when {{ fail_calc }} {{ warn_if }}
-        then 'true' else 'false' end as should_warn,
-      case when {{ fail_calc }} {{ error_if }}
-        then 'true' else 'false' end as should_error
-    from (
-      select * from {{testview}}
-    ) dbt_internal_test;
+  EXEC('drop view {{testview}};')
 
-    EXEC('drop view {{testview}};')
-
-  {% else -%}
-    select
-      {{ "top (" ~ limit ~ ')' if limit != none }}
-      {{ fail_calc }} as failures,
-      case when {{ fail_calc }} {{ warn_if }}
-        then 'true' else 'false' end as should_warn,
-      case when {{ fail_calc }} {{ error_if }}
-        then 'true' else 'false' end as should_error
-    from (
-      {{ main_sql }}
-    ) dbt_internal_test
-  {%- endif -%}
 {%- endmacro %}
