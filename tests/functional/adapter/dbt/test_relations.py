6f3c9de5fbdf85316707c167b9310088f50383a0
Axell Padilla <68310020+axellpadilla@users.noreply.github.com>
2024-09-28 05:49:45 +0000

    fix to make drop_schema_name usable and test pass

--- tests/functional/adapter/dbt/test_relations.py
+++ tests/functional/adapter/dbt/test_relations.py
@@ -1,4 +1,3 @@
-import pytest
 from dbt.tests.adapter.relations.test_changing_relation_type import BaseChangeRelationTypeValidator
 from dbt.tests.adapter.relations.test_dropping_schema_named import BaseDropSchemaNamed
 
@@ -7,12 +6,5 @@ class TestChangeRelationTypeValidator(BaseChangeRelationTypeValidator):
     pass
 
 
-@pytest.mark.xfail(
-    reason="""
-                  Test fails as its not passing Use[] properly.
-                  `Use[None]` is called, should be `User[TestDB]`
-                  Unclear why the macro doens't pass it properly.
-                  """
-)
 class TestDropSchemaNamed(BaseDropSchemaNamed):
     pass
