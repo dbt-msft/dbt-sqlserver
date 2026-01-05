25f548e2635ebab1d63b10624502539f22840cc7
Ben Knight <Benjamin.Knight@wvt.nhs.uk>
2026-01-05 16:25:18 +0000

    Add python 3.13 to more required locations.

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     project_urls={
         "Setup & configuration": "https://docs.getdbt.com/reference/warehouse-profiles/mssql-profile",  # noqa: E501
