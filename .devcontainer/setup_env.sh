8dd96007b8c6ef31c0f9e6e279e441362f5044e7
Ben Knight <Benjamin.Knight@wvt.nhs.uk>
2026-05-20 09:42:18 +0000

    Fix an issue where the env setup assumed the venv existed. Swap to uv sync over pip install as we are now using uv and include the uv lock for reproducible setups.

--- .devcontainer/setup_env.sh
+++ .devcontainer/setup_env.sh
@@ -9,6 +9,8 @@ docker compose up -d
 pip install uv
 
 # Use a workspace-local virtualenv so package installs do not fail on user permissions.
-uv pip install -e . --group dev
+[ -d .venv ] || uv venv
 source .venv/bin/activate
+
+uv sync --group dev --extra pyodbc
 pre-commit install
