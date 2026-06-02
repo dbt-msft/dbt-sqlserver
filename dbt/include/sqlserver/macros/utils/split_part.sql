c32b280172db7a962c2c61c42c6146b99e5f100a
Josh Markovic <jmarkovic@newhollandcapital.com>
2026-06-02 21:10:21 +0000

    Fix Spelling

--- dbt/include/sqlserver/macros/utils/split_part.sql
+++ dbt/include/sqlserver/macros/utils/split_part.sql
@@ -2,7 +2,7 @@
 	For more information on how this XML trick works with splitting strings, see https://www.sqlservertips.com/sqlservertip/1771/splitting-delimited-strings-using-xml-in-sql-server/
 	On Azure SQL and SQL Server 2019, we can use the string_split function instead of the XML trick.
 	But since we don't know which version of SQL Server the user is using, we'll stick with the XML trick in this adapter.
-	However, since the XML data type is not supported in Synapse, it has to be overriden in that adapter.
+	However, since the XML data type is not supported in Synapse, it has to be overridden in that adapter.
 
     To adjust for negative part numbers, aka 'from the end of the split', we take the position and subtract from last to get the specific part.
     Since the input is '-1' for the last, '-2' for second last, we add 1 to the part number to get the correct position.
